82bd16bcf8b2621f6dbd6fb4b1538d5f60b2967f
alejandro <aguerreroegido@gmail.com>
2026-09-04 10:32:59 +0100

    Add Maven wrapper, initial Spring Boot project setup, and sample configuration files

---
+++ src/main/java/com/alejandro/mtoplatform/MtoPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.mtoplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtoPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtoPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alejandro/mtoplatform/MtoPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.mtoplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtoPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
